77d9d417d2eddae2df72d85f645e4883c7912706
noahstrac <nstracqu@uvm.edu>
2020-02-20 15:55:14 -0500

    finished first draft of branch.h and .cpp

--- branch.cpp
+++ branch.cpp
@@ -1,4 +1,52 @@
-//
-// Created by Noah Stracqualursi on 2/20/20.
-//
+#include "branch.h"
+
+/* constructors */
+Branch::Branch(){
+    std::string f_branch_name = "";
+    std::string f_branch_manager = "";
+    std::vector<Account> f_accounts = {};
+}
+
+Branch::Branch(std::string p_branch_name, std::string p_branch_manager, std::vector<Account> p_accounts){
+    std::string f_branch_name = p_branch_name;
+    std::string f_branch_manager = p_branch_manager;
+    std::vector<Account> f_accounts = p_accounts;
+}
+
+Branch::Branch(std::string p_branch_name, std::string p_branch_manager){
+    std::string f_branch_name = p_branch_name;
+    std::string f_branch_manager = p_branch_manager;
+    std::vector<Account> f_accounts = {};
+}
+
+/* getters */
+std::string Branch::get_branch_name(){
+    return f_branch_name;
+}
+
+std::string Branch::get_branch_manager(){
+    return f_branch_manager;
+}
+
+std::vector<Account> Branch::get_accounts(){
+    return f_accounts;
+}
+
+/* setters */
+void Branch::set_branch_name(std::string p_branch_name){
+    f_branch_name = p_branch_name;
+}
+
+void Branch::set_branch_manager(std::string p_branch_manager){
+    f_branch_manager = p_branch_manager;
+}
+
+void Branch::set_accounts(std::vector<Account> p_accounts){
+    f_accounts = p_accounts;
+}
+
+/* functions */
+void Branch::add_account(Account p_account){
+    f_accounts.push_back(p_account);
+}
 
--- branch.h
+++ branch.h
@@ -3,9 +3,33 @@
 #define NJ_NS_PROJECT3_BRANCH_H
 
 #include "bank.h"
+#include "account.h"
+#include <vector>
 
 class Branch : public Bank{
 protected:
+    std::string f_branch_name;
+    std::string f_branch_manager;
+    std::vector<Account> f_accounts;
+public:
+
+    /* constructors */
+    Branch();
+    Branch(std::string p_branch_name, std::string p_branch_manager, std::vector<Account> p_accounts);
+    Branch(std::string p_branch_name, std::string p_branch_manager);
+
+    /* getters */
+    std::string get_branch_name();
+    std::string get_branch_manager();
+    std::vector<Account> get_accounts();
+
+    /* setters */
+    void set_branch_name(std::string p_branch_name);
+    void set_branch_manager(std::string p_branch_manager);
+    void set_accounts(std::vector<Account> p_accounts);
+
+    /* functions */
+    void add_account(Account p_account);
 
 };
 #endif //NJ_NS_PROJECT3_BRANCH_H
